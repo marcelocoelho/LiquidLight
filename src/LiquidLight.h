2ccf08bbd8be367361697ba4ff7db0f7eee74962
Marcelo Coelho <marcelocoelho@marcelos-mbp.lan>
2018-06-03 09:32:25 -0400

    All key elements in place
    Got interrupt driven animation working, while interrupting it with IR
    remote commands without a problem.

--- src/LiquidLight.h
+++ src/LiquidLight.h
@@ -1,5 +1,23 @@
 
 
+// State machine and display settings
+///////////////////////////////////////////////////////////////////////////////////
+enum State {
+   WAITING,
+   PLAYING,
+   DEBUG
+};
+
+int globalState = WAITING;
+
+volatile unsigned char displayFramerateDivider;
+
+#define FPS_15  0x0F  // 0x0F means 66 miliseconds in between frames, so 15 frames a second
+#define FPS_30  0x07  // 0x07 means 33 miliseconds in between frames, 30 frames a second
+#define FPS_60  0x03  // 0x03 means 16 or 17 miliseconds in between frames so 60 frames a second
+
+long countdownFrames = 0;
+
 
 
 // IR Remote
@@ -16,7 +34,7 @@ decode_results results;
 // LED Strip
 ///////////////////////////////////////////////////////////////////////////////////
 #include <FastLED.h>
-#define LEDS_NUM          3
+#define LEDS_NUM          10
 
 #define LEDS_DATA         5
 #define LEDS_CLOCK        4
--- src/LiquidLight.ino
+++ src/LiquidLight.ino
@@ -1,12 +1,13 @@
 
+// FOR SOME REASON, MAIN INTERRUPT ROUTINE STOPPED RUNNING AND IR STOPPED RUNNING AS WELL
+
 #include "LiquidLight.h"
-
-
 
 
 
 void setup() {
 
+  initDebugLED();
 
   #ifdef DEBUG_ENABLED
   initSerial();
@@ -16,10 +17,66 @@ void setup() {
 
   initLEDStrip();
 
-  LEDStripFullOn();
+  displayStartFrameInterrupt();
 
 }
 
+
+
+/*//////////////////////////////////////////////////////////////////////////////////
+    TIMER INTERRUPT AND MAIN ROUTINES
+    Time gets called repeatedly, every 4 ms.
+//////////////////////////////////////////////////////////////////////////////////*/
+ISR(TIMER1_OVF_vect) {
+
+  //debugOn();
+  //debugOff();
+
+  // GO GET INPUT
+  getInput();
+
+  // THEN DEAL WITH OUTPUT
+  displayFramerateDivider = (++displayFramerateDivider) & FPS_30; // this counts from 0 incrementally by 1
+
+  if(displayFramerateDivider == 0)   // this counts frames
+  {
+
+    if (countdownFrames > 0) {            // If there is a time, count down to zero
+      countdownFrames--;
+    } else if (countdownFrames == 0) {   // When it gets to 0, then play animation
+      setDisplay();
+    } else if (countdownFrames < 0) {   // But if it's -1, then ignore this animation and don't play anything
+      // do nothing
+    }
+  }
+}
+
+
+
+/*//////////////////////////////////////////////////////////////////////////////////
+    TIMER INTERRUPTS
+//////////////////////////////////////////////////////////////////////////////////*/
+
+//
+// displayStartFrameInterrupt() : Sets up the microcontroller to interrupt when Timer1 overflows.
+//
+void displayStartFrameInterrupt()
+{
+   TIMSK1 = _BV(TOIE1);
+   //sei();
+   TCCR1B |= 0x02;
+
+   sei();
+}
+
+
+//
+// displayStopFrameInterrupt() : Keeps the microcontroller from interrupting on Timer1 overflow.
+//
+void displayStopFrameInterrupt()
+{
+   TIMSK1 = ~_BV(TOIE1);
+}
 
 
 
@@ -31,6 +88,33 @@ void initSerial() {
   Serial.begin(115200);
   //delay(3000);
   debug("Liquid Light v1.0");
+}
+
+void initDebugLED()
+{
+	pinMode(DEBUG_LED, OUTPUT);
+	debugOff();
+}
+
+void debugOn()
+{
+	digitalWrite(DEBUG_LED, HIGH);
+}
+
+
+void debugLow() {
+  digitalWrite(DEBUG_LED, LOW);
+}
+
+void debugOff()
+{
+  digitalWrite(DEBUG_LED, LOW);
+}
+
+void debugFlicker() {
+  digitalWrite(DEBUG_LED, HIGH);
+  delay(5);
+  digitalWrite(DEBUG_LED, LOW);
 }
 
 
@@ -53,7 +137,7 @@ void LEDStripFullOn()
     leds[j] = CRGB(255);
   }
   FastLED.show();
-  FastLED.show();
+  //FastLED.show();
 }
 
 void LEDStripFullOff()
@@ -63,7 +147,7 @@ void LEDStripFullOff()
     leds[j] = CRGB(0);
   }
   FastLED.show();
-  FastLED.show();
+  //FastLED.show();
 }
 
 
@@ -82,6 +166,7 @@ void getInput() {
     switch (command) {
       case 2011249228:  //CENTER
         Serial.println("center");
+        globalState = PLAYING;
         //displayAnimation();
       break;
 
@@ -123,11 +208,68 @@ void getInput() {
 
 
 /*//////////////////////////////////////////////////////////////////////////////////
+    DISPLAY LOGIC
+
+//////////////////////////////////////////////////////////////////////////////////*/
+
+int positionCounter = 0;
+int slower = 0; // weird hack to slow down animation by skipping frames
+
+void setDisplay() {
+
+  slower++;
+
+  if (globalState == PLAYING && slower > 100) {
+
+    for (int i = 0; i < LEDS_NUM; i++) {    //clear strip
+      leds[i] =  CRGB(0);
+    }
+
+
+    leds[positionCounter] =  CRGB(255);
+    positionCounter++;
+
+    if(positionCounter >= LEDS_NUM - 1) {
+      positionCounter = 0;
+      globalState = WAITING;
+    }
+
+    FastLED.show();
+
+    slower = 0;
+  }
+
+
+  /*
+  if (globalState == PLAYING) {
+
+    int gotData = 0;
+
+    gotData = myFile.read(buf, sizeof(buf));      // grabs one line at a time from memory card
+    if (gotData != 0) {
+      displayLine();
+    } else {
+      globalState = WAITING;
+      //myFile.seek(0);
+      myFile.close();
+    }
+  }
+  */
+}
+
+
+
+
+
+
+
+
+/*//////////////////////////////////////////////////////////////////////////////////
     LOOP
 //////////////////////////////////////////////////////////////////////////////////*/
 void loop()
 {
   // does nothing
-  getInput();
+  //getInput();
 
 }
